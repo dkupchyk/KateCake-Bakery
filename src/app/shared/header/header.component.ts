6d422d9f262b310581b8c72ec3ff8ae920307627
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2020-08-26 17:06:14 +0300

    Implemented responsive header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
+  screenWidth: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event): void {
+    this.screenWidth = event.target.innerWidth;
+  }
+
+  openNav(): void {
+    (document.getElementById('header-shrinked') as HTMLElement).style.width = '100vw';
+  }
+
+  closeNav(): void {
+    (document.getElementById('header-shrinked') as HTMLElement).style.width = '0';
+  }
+
+  changePath(path: string): void {
+    this.closeNav();
+    this.router.navigate([path]);
+  }
 }
