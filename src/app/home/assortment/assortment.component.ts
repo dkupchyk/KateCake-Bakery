3968a5d68a61d9b81a25c7640cfa68db3cc9816b
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2020-08-28 17:43:03 +0300

    Implementd overlay to the assortment photos

--- src/app/home/assortment/assortment.component.ts
+++ src/app/home/assortment/assortment.component.ts
@@ -7,11 +7,31 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AssortmentComponent implements OnInit {
   assortmentArray = [
-    { title: 'Торты', imagePath: 'assets/images/main/assortment-cakes.png' },
-    { title: 'Чизкейки', imagePath: 'assets/images/main/assortment-cheesecakes.png' },
-    { title: 'Капкейки', imagePath: 'assets/images/main/assortment-cupcakes.png' },
-    { title: 'Макароны', imagePath: 'assets/images/main/assortment-macarons.png' },
-    { title: 'Эскимо-кейкпопсы', imagePath: 'assets/images/main/assortment-eskimo.png' },
+    {
+      title: 'Торты',
+      imagePath: 'assets/images/main/assortment-cakes.png',
+      hoverImagePath: 'assets/images/main/assortment-cakes-overlay.png'
+    },
+    {
+      title: 'Чизкейки',
+      imagePath: 'assets/images/main/assortment-cheesecakes.png',
+      hoverImagePath: 'assets/images/main/assortment-cheesecakes-overlay.png'
+    },
+    {
+      title: 'Капкейки',
+      imagePath: 'assets/images/main/assortment-cupcakes.png',
+      hoverImagePath: 'assets/images/main/assortment-cupcakes-overlay.png'
+    },
+    {
+      title: 'Макароны',
+      imagePath: 'assets/images/main/assortment-macarons.png',
+      hoverImagePath: 'assets/images/main/assortment-macarons-overlay.png'
+    },
+    {
+      title: 'Эскимо-кейкпопсы',
+      imagePath: 'assets/images/main/assortment-eskimo.png',
+      hoverImagePath: 'assets/images/main/assortment-eskimo-overlay.png'
+    },
 
   ];
 
